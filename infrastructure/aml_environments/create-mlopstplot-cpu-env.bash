50e3c2aca3a5a2cbb5ea7b3bd0f8ca9337e773ed
Gabriel2409 <gabriel.cournelle@gmail.com>
2023-08-08 10:47:14 +0200

    updated script

--- infrastructure/aml_environments/create-mlopstplot-cpu-env.bash
+++ infrastructure/aml_environments/create-mlopstplot-cpu-env.bash
@@ -1,11 +1,14 @@
 #!/bin/bash
+
 AZURE_RG="$1"
 AZUREML_WORKSPACE="$2"
+
+script_dir="$(cd "$(dirname "$0")" && pwd)"
 
 az ml environment create \
 --name mlopstplt-cpu-env \
 --resource-group $AZURE_RG \
 --workspace-name $AZUREML_WORKSPACE \
 --image mcr.microsoft.com/azureml/openmpi4.1.0-ubuntu20.04:latest \
---conda-file conda-dependencies-cpu.yml
+--conda-file $script_dir/conda-dependencies-cpu.yml
 

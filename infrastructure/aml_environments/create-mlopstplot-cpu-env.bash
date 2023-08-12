4c5879f43cbaa5c993de77d0bd8e38a5646c685b
Gabriel2409 <gabriel.cournelle@gmail.com>
2023-08-12 16:50:03 +0200

    removed some vars

--- infrastructure/aml_environments/create-mlopstplot-cpu-env.bash
+++ infrastructure/aml_environments/create-mlopstplot-cpu-env.bash
@@ -2,11 +2,12 @@
 
 AZURE_RG="$1"
 AZUREML_WORKSPACE="$2"
+AML_CPU_ENV="$3"
 
 script_dir="$(cd "$(dirname "$0")" && pwd)"
 
 az ml environment create \
---name mlopstplt-cpu-env \
+--name $AML_CPU_ENV \
 --resource-group $AZURE_RG \
 --workspace-name $AZUREML_WORKSPACE \
 --image mcr.microsoft.com/azureml/openmpi4.1.0-ubuntu20.04:latest \
